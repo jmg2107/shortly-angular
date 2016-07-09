e36e5e6140a3496b63f5a076f9c7ee50eacc9cd0
jmg2107 <jenjengoo@gmail.com>
2016-07-09 10:17:53 -0700

    completed all tests

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,31 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+// Simple GET request example:
+  var getAll = function () {
+    console.log ("FLY NYAN FLY");
+      return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function (res) {
+      return res.data;
+    });
+  };
+  var addOne = function (link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: link
+    })
+    .then(function (resp) {
+      return resp;
+    });
+  };
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,11 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function (link) {
+    Links.addOne(link)
+    .then(function () {
+      console.log("posted new link");
+    });
+  };
 });
